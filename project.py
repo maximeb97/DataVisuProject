bf8259a57d26221844bfdc6e9f52fecf38d082c9
Maxime Betemps <maxime.betemps@epitech.eu>
2020-02-01 19:50:13 +0100

    up

--- project.py
+++ project.py
@@ -2,15 +2,6 @@ import plotly.express as px
 import pandas
 import matplotlib.pyplot as plt
 
-labels = {
-    "year": "Année",
-    "npg": "npg",
-    "birth_rate": "Taux de naissance",
-    "death_rate": "Taux de decès",
-    "gdw": "Poids démographique",
-    "urbanization": "Urbanisation"
-}
-
 
 def main():
     visu = select_visu()
@@ -18,7 +9,7 @@ def main():
         print("Undefined visualization, please select amoung the possible visualizations")
         visu = select_visu()
     if (visu == "1"):
-        print("visu 1")
+        visu_parallel_coordinates()
     elif (visu == "2"):
         print("visu 2")
     elif (visu == "3"):
@@ -26,17 +17,38 @@ def main():
     exit(0)
 
 
-# df = pandas.read_csv("russian_demography.csv", usecols=[
-#     "year", "birth_rate", "death_rate", "gdw", "urbanization"])
+def visu_parallel_coordinates():
+    labels = {
+        "year": "Année",
+        "npg": "Variation naturelle",
+        "birth_rate": "Taux de naissance",
+        "death_rate": "Taux de decès",
+        "gdw": "Poids démographique",
+        "urbanization": "Urbanisation"
+    }
+    df = pandas.read_csv("russian_demography.csv", usecols=[
+        "year", "birth_rate", "death_rate", "gdw", "urbanization", "npg"])
 
-# fig = px.parallel_coordinates(df,
-#                               color="year",
-#                               labels=labels,
-#                               color_continuous_scale=px.colors.diverging.Tealrose,
-#                               color_continuous_midpoint=2,
-#                               range_color=[1990, 2017])
-# fig.show()
-# # fig.write_image("parallel_coordinate_plot.pdf")
+    fig = px.parallel_coordinates(df,
+                                  color="year",
+                                  labels=labels,
+                                  color_continuous_scale=px.colors.diverging.Tealrose,
+                                  color_continuous_midpoint=2,
+                                  range_color=[1990, 2017])
+    fig.show()
+
+
+def visu2():
+    cat = ["bored", "happy", "bored", "bored", "happy", "bored"]
+    dog = ["happy", "happy", "happy", "happy", "bored", "bored"]
+    activity = ["combing", "drinking", "feeding",
+                "napping", "playing", "washing"]
+    fig, ax = plt.subplots()
+    ax.plot(activity, dog, label="dog")
+    # ax.plot(activity, cat, label="cat")
+    ax.legend()
+
+    plt.show()
 
 
 def select_visu():
