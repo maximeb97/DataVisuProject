a8056a636ff3a47a771548d84240a9d407383b1c
Maxime Betemps <maxime.betemps@epitech.eu>
2020-02-01 11:56:09 +0100

    help me

--- project.py
+++ project.py
@@ -2,9 +2,17 @@ import plotly.express as px
 import pandas
 import matplotlib.pyplot as plt
 
-labels={"year": "Year", "npg": "npg", "birth_rate": "Taux de naissance", "death_rate": "Taux de decès"}
+labels = {
+    "year": "Année",
+    "npg": "npg",
+    "birth_rate": "Taux de naissance",
+    "death_rate": "Taux de decès",
+    "gdw": "Poids démographique",
+    "urbanization": "Urbanisation"
+}
 
-df = pandas.read_csv("russian_demography.csv", usecols=["year", "birth_rate", "death_rate", "gdw", "urbanization"])
+df = pandas.read_csv("russian_demography.csv", usecols=[
+                     "year", "birth_rate", "death_rate", "gdw", "urbanization"])
 
 fig = px.parallel_coordinates(df,
                               color="year",
@@ -14,3 +22,13 @@ fig = px.parallel_coordinates(df,
                               range_color=[1990, 2017])
 fig.show()
 # fig.write_image("parallel_coordinate_plot.pdf")
+
+
+def print_help():
+    print("[Russian Demogaphy Data]")
+    print("----------------------------------------------------")
+    print("Enter one of the following number to visualize data:")
+    print("1 - Display the parallel coordinates")
+    print("2 - ")
+    print("----------------------------------------------------")
+    return input()
